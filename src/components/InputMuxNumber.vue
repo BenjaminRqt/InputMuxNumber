da48d4059fb88b8585b0aedc058bcf1b4c464d14
Benjamin Rouquet <benjamin.rouquet4@gmail.com>
2020-03-01 12:39:52 +0100

    texte help min et max

--- src/components/InputMuxNumber.vue
+++ src/components/InputMuxNumber.vue
@@ -26,106 +26,126 @@
                     @touchcancel="stop"
             >+</button>
         </span>
+        <span class="help" v-if="hasHelp">{{ textHelp }}</span>
     </div>
 </template>
 <script lang="ts">
 /* eslint-disable */
 
-  export default {
-  name: 'InputMuxNumber',
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    min: {
-      type: Number,
-    },
-    max: {
-      type: Number,
-    },
-    label: {
-      type: String,
-    },
-    speed: {
-        type: Number,
-        default: 60
-    }
-  },
-  data() {
-    return {
-        value: 0,
-        interval:false,
-        counter: 0
-    }
-  },
-    created() {
-        if(this.min) {
-            this.value = this.min;
+    export default {
+        name: 'InputMuxNumber',
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            min: {
+                type: Number,
+            },
+            max: {
+                type: Number,
+            },
+            label: {
+                type: String,
+            },
+            speed: {
+                type: Number,
+                default: 60
+            },
+            help: {
+                type: Boolean,
+                default: true
+            }
+        },
+        data() {
+            return {
+                value: 0,
+                interval: false,
+                counter: 0,
+                hasHelp: this.help
+            }
+        },
+        created() {
+            if (this.min) {
+                this.value = this.min;
+            }
+        },
+        methods: {
+            down() {
+                if (!this.interval) {
+                    this.interval = setInterval(() => {
+                        if (!this.respectMin(this.value - this.getMultiplier())) {
+                            this.value = this.min;
+                        } else if (this.respectMin()) {
+                            this.value -= this.getMultiplier();
+                            ++this.counter;
+                        }
+                    }, this.speed);
+                }
+            },
+            up() {
+                if (!this.interval) {
+                    this.interval = setInterval(() => {
+                        if (!this.respectMax(this.value + this.getMultiplier())) {
+                            this.value = this.max;
+                        } else if (this.respectMax()) {
+                            this.value += this.getMultiplier();
+                            ++this.counter;
+                        }
+                    }, this.speed);
+                }
+            },
+            stop() {
+                clearInterval(this.interval);
+                this.interval = false;
+                this.counter = 0;
+            },
+            respectMin(value = null) {
+                if (value) {
+                    return this.min < value;
+                }
+                return this.min < this.value;
+            },
+            respectMax(value = null) {
+                if (value) {
+                    return this.max > value;
+                }
+                return this.max > this.value;
+            },
+            getMultiplier() {
+                if (this.counter < 10) {
+                    return 1;
+                } else if (this.counter >= 10 && this.counter < 50) {
+                    return 10;
+                } else if (this.counter >= 50 && this.counter < 100) {
+                    return 100
+                } else {
+                    return 1000;
+                }
+            }
+        },
+        computed: {
+            textHelp() {
+                if(this.min && !this.max) {
+                    return `+ de ${this.min}`;
+                } else if(!this.min && this.max) {
+                    return `- de ${this.max}`;
+                } else if(this.min && this.max) {
+                    return `entre ${this.min} et ${this.max}`;
+                } else {
+                    this.hasHelp = false;
+                    return '';
+                }
+            }
         }
-    },
-  methods: {
-      down() {
-          if(!this.interval){
-              this.interval = setInterval(() => {
-                  if(!this.respectMin(this.value - this.getMultiplier())) {
-                      this.value = this.min;
-                  } else if(this.respectMin()) {
-                      this.value -= this.getMultiplier();
-                      ++this.counter;
-                  }
-              }, this.speed);
-          }
-      },
-      up(){
-          if(!this.interval){
-              this.interval = setInterval(() => {
-                  if(!this.respectMax(this.value + this.getMultiplier())) {
-                    this.value = this.max;
-                  } else if(this.respectMax()) {
-                      this.value += this.getMultiplier();
-                      ++this.counter;
-                  }
-              }, this.speed);
-          }
-      },
-      stop(){
-          clearInterval(this.interval);
-          this.interval = false;
-          this.counter = 0;
-      },
-      respectMin(value = null) {
-          if(value) {
-              return this.min < value;
-          }
-          return this.min < this.value;
-      },
-      respectMax(value = null) {
-          if(value) {
-              return this.max > value;
-          }
-          return this.max > this.value;
-      },
-      getMultiplier() {
-          if(this.counter < 10) {
-              return 1;
-          } else if (this.counter >= 10 && this.counter < 50) {
-              return 10;
-          } else if (this.counter >= 50  && this.counter < 100) {
-              return 100
-          } else {
-              return 1000;
-          }
-      }
-  }
-};
+    };
 </script>
 
 <style lang="css" scoped>
-  .input-nux-number > .icons i {
-    font-weight: bold;
-      cursor: pointer;
-      font-size: 1.5rem;
-      padding: 4px;
-  }
+    .input-nux-number > .icons i {
+        font-weight: bold;
+        cursor: pointer;
+        font-size: 1.5rem;
+        padding: 4px;
+    }
 </style>
